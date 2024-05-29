1c28c49dd8b038305c573251117349fd4c7aca53
samwhelp <samwhelp@users.noreply.github.com>
2024-05-29 17:54:08 +0800

    fix

---
+++ prototype/main/demo-config/keybind/demo-keybind-mode-dolphin/config-install.sh
@@ -0,0 +1,149 @@
+#!/usr/bin/env bash
+
+set -e
+
+
+################################################################################
+### Head: kde
+##
+
+kde_config_run_pre () {
+
+	#kde_service_stop
+
+
+	return 0
+
+}
+
+kde_config_run_post () {
+
+	#kde_service_start
+
+
+	return 0
+
+}
+
+kde_service_stop () {
+
+	systemctl --user stop plasma-kglobalaccel.service
+
+
+	systemctl --user stop plasma-kactivitymanagerd.service
+
+	rm ~/.local/share/kactivitymanagerd/ -rf
+
+
+	systemctl --user stop plasma-plasmashell.service
+
+
+	return 0
+
+}
+
+kde_service_start () {
+
+	systemctl --user start plasma-kglobalaccel.service
+
+
+	systemctl --user start plasma-plasmashell.service
+
+
+	systemctl --user start plasma-kactivitymanagerd.service
+
+
+	return 0
+
+}
+
+kde_config_install () {
+
+	echo
+	echo "##"
+	echo "## Config: kde"
+	echo "##"
+	echo
+
+
+	kde_config_install_by_dir
+
+	#kde_config_install_by_each_file
+
+
+	echo
+
+}
+
+kde_config_install_by_dir () {
+
+
+	echo
+	echo "mkdir -p ${HOME}"
+	mkdir -p "${HOME}"
+
+
+	echo
+	echo "cp -rf ./asset/overlay/etc/skel/. ${HOME}"
+	cp -rf "./asset/overlay/etc/skel/." "${HOME}"
+
+
+}
+
+
+kde_config_install_by_each_file () {
+
+
+	echo
+	echo "mkdir -p ${HOME}/.config"
+	mkdir -p "${HOME}/.config"
+
+	echo
+	echo "install -Dm644 ./asset/overlay/etc/skel/.config/kglobalshortcutsrc ${HOME}/.config/kglobalshortcutsrc"
+	install -Dm644 "./asset/overlay/etc/skel/.config/kglobalshortcutsrc" "${HOME}/.config/kglobalshortcutsrc"
+
+
+}
+
+##
+### Tail: kde
+################################################################################
+
+
+################################################################################
+### Head: config_install
+##
+
+main_config_install () {
+
+	kde_config_run_pre
+
+	kde_config_install
+
+	kde_config_run_post
+
+}
+
+##
+### Tail: config_install
+################################################################################
+
+
+################################################################################
+### Head: Main
+##
+
+__main__ () {
+
+	main_config_install
+
+}
+
+##
+## Start
+##
+__main__
+
+##
+### Tail: Main
+################################################################################
